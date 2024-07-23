a009b01d89aee3d20342a992b4c34317dfea4294
Umesh Nagare <umeshnagare27@gmail.com>
2024-07-23 20:31:10 +0530

    Remove Nth Node in LL from Last

---
+++ Java2/LinkedList/src/Node.java
@@ -0,0 +1,11 @@
+public class Node {
+    int data;
+    Node pudcha;
+    Node(int data) {
+        this.data = data;
+        this.pudcha = null;
+    }
+    Node(){
+        this.pudcha=null;
+    };
+}
