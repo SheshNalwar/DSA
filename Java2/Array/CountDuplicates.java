434c2c0cb156af4be873f4d0ec71da5664011b12
Umesh Nagare <umeshnagare27@gmail.com>
2024-07-29 14:22:47 +0530

    Some File Name Changes

--- Java2/Array/CountDuplicates.java
+++ Java2/Array/CountDuplicates.java
@@ -1,9 +1,12 @@
 
-public class RemoveDuplicates {
+public class CountDuplicates {
     public static void main(String[] args) {
         int arr[] = { 1, 1, 2, 2, 2, 3, 4, 5, 6, 7, 7, 8 };
         int k = removeDuplicates(arr);
         System.out.println("No. of Elements after removal of duplicates are " + k);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 
     static int removeDuplicates(int arr[]) {
