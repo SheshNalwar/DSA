35ce69086dcf35c2ef883eadbe53a794f703aca3
SheshNalwar <nalwarshesh@gmail.com>
2024-08-26 20:54:38 +0530

    Implement Stack Using queue

---
+++ Java/Stack/Stack_Easy/ImplementStackUsingQueue.java
@@ -0,0 +1,77 @@
+/*
+225. Implement Stack using Queues
+
+ * Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+
+Notes:
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+ 
+Example 1:
+Input
+["MyStack", "push", "push", "top", "pop", "empty"]
+[[], [1], [2], [], [], []]
+Output
+[null, null, null, 2, 2, false]
+
+Explanation
+MyStack myStack = new MyStack();
+myStack.push(1);
+myStack.push(2);
+myStack.top(); // return 2
+myStack.pop(); // return 2
+myStack.empty(); // return False
+ */
+
+package Java.Stack.Stack_Easy;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueue {
+
+    Queue<Integer> q1;
+    Queue<Integer> q2;
+
+    public ImplementStackUsingQueue() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        q1.add(x);
+        while (!q2.isEmpty()) {
+            q1.add(q2.remove());
+        }
+    }
+
+    public int pop() {
+        return q1.remove();
+    }
+
+    public int top() {
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+
+    /**
+     * Your MyStack object will be instantiated and called as such:
+     * MyStack obj = new MyStack();
+     * obj.push(x);
+     * int param_2 = obj.pop();
+     * int param_3 = obj.top();
+     * boolean param_4 = obj.empty();
+     */
+}
