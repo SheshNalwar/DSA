606ce18ac37a582028bf37360ea54ee5baf71c6f
Umesh Nagare <umeshnagare27@gmail.com>
2024-09-05 22:57:24 +0530

    fibo

--- Java2/Strings/src/RemoveSpaces.java
+++ Java2/Strings/src/RemoveSpaces.java
@@ -11,13 +11,14 @@ public class RemoveSpaces {
     }
 
     public static void main(String[] args) {
-//        String result = removeSpaces("umesh nagare 27 @ gmail . com");
-//        System.out.println(result);
+        String result = removeSpaces("umesh nagare 27 @ gmail . com");
+        System.out.println(result);
         String name = "Umesh Nagare studies in AG Patil";
         String[] resulted = name.split(" ");
         String words = "";
         for (int i = 0; i < resulted.length; i++) {
             words+=resulted[i];
         }
+        System.out.println(words);
     }
 }
