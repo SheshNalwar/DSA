4d2c41bd01a70f7a1beaabdd1c89c39eb9ca0868
Umesh Nagare <umeshnagare27@gmail.com>
2024-09-08 17:48:18 +0530

    String concepts and questions

---
+++ Java2/Strings/src/PrettyPrinting.java
@@ -0,0 +1,13 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 252.4478f;
+        System.out.printf("%.3f",a);
+        System.out.println();
+        System.out.printf("%.2f",Math.PI);
+        System.out.println();
+//        Format Specifiers
+        System.out.printf("Hello my name is %s and my family name is %s","Umesh","Nagare");
+        System.out.println();
+        System.out.printf("The value is %d",27);
+    }
+}
---
+++ Java2/Strings/src/StringBegin.java
@@ -0,0 +1,6 @@
+public class StringBegin {
+    public static void main(String[] args) {
+        String name = "Umesh Nagare";
+
+    }
+}
