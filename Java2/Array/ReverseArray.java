2a7b39403869b6527e452d0f150d374331d12265
Umesh Nagare <umeshnagare27@gmail.com>
2024-09-13 11:17:34 +0530

    Reverse an array using recursion

--- Java2/Array/ReverseArray.java
+++ Java2/Array/ReverseArray.java
@@ -3,18 +3,29 @@ import java.util.Arrays;
 public class ReverseArray {
     public static void main(String[] args) {
         int arr[] = { 1, 2, 3, 4, 5 };
-        int start = 0;
-        int end = arr.length-1;
-        while (start<end){
-            swap(arr,start,end);
-            start++;
-            end--;
-        }
+        recursionReverse(arr,0, arr.length-1);
+//        twoPointerReverse(arr);
         System.out.println(Arrays.toString(arr));
     }
     static void swap(int arr[],int index1,int index2){
         int temp = arr[index1];
         arr[index1]=arr[index2];
         arr[index2]=temp;
+    }
+    static void twoPointerReverse(int arr[]){
+        int start=0;
+        int end = arr.length-1;
+        while (start<end){
+            swap(arr,start,end);
+            start++;
+            end--;
+        }
+    }
+    static void recursionReverse(int arr[],int start,int end){
+        if (start>=end){
+            return;
+        }
+        swap(arr, start, end);
+        recursionReverse(arr,start+1,end-1);
     }
 }
