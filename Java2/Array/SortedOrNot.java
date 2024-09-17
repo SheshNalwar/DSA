e2097164fba19fcfd30d5bd2f0bd0149b372005c
Umesh Nagare <umeshnagare27@gmail.com>
2024-09-17 20:39:08 +0530

    sorted or not using recursion

--- Java2/Array/SortedOrNot.java
+++ Java2/Array/SortedOrNot.java
@@ -3,7 +3,9 @@ public class SortedOrNot {
         int ar[]={1,2,3,4,5};
         int ar2[]={2,1,2,3,4,5};
         System.out.println(sortedOrNot(ar));
+        System.out.println(recursionSortorNot(ar,0));
         System.out.println(sortedOrNot(ar2));
+        System.out.println(recursionSortorNot(ar2,0));
     }
     static boolean sortedOrNot(int arry[]){
         int notSorted = 0;
@@ -16,5 +18,11 @@ public class SortedOrNot {
             }
         }
         return true;
+    }
+    static boolean recursionSortorNot(int arry[],int i){
+        if (i== arry.length-1){
+            return true;
+        }
+        return arry[i]<arry[i+1] && recursionSortorNot(arry,i+1);
     }
 }
