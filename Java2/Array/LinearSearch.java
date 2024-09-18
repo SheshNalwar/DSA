b591ae56f287f052fce8523244cc30afb27c60b0
Umesh Nagare <umeshnagare27@gmail.com>
2024-09-18 20:38:09 +0530

    Linear search using recursion

--- Java2/Array/LinearSearch.java
+++ Java2/Array/LinearSearch.java
@@ -3,11 +3,13 @@ public class LinearSearch {
         int arr[] = { 10, 225, 32, 40,45,21,22,27 };
         String name = "Umesh";
         char [] chars = name.toCharArray();
-        for (int i = 0; i < name.length(); i++) {
-            System.out.print(chars[i]);
-        }
+//        for (int i = 0; i < name.length(); i++) {
+//            System.out.print(chars[i]);
+//        }
 //        System.out.println(search(name,'e'));
-        System.out.println(search(arr,27));
+//        System.out.println(search(arr,27));
+        int result = recurseSearch(arr,40,0);
+        System.out.println(result);
     }
     static int search(int arr[],int key){
         for (int i = 0; i < arr.length; i++) {
@@ -25,5 +27,12 @@ public class LinearSearch {
         }
         return -1;
     }
-
+    static int recurseSearch(int arr[],int target,int index){
+        if (index==arr.length-1 && arr[index]!=target){
+            return -1;
+        }else if(arr[index]==target) {
+            return index;
+        }
+        return recurseSearch(arr, target, index+1);
+    }
 }
