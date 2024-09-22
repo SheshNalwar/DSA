cd8807f560433cbda434b03d8841900229cdc130
SheshNalwar <nalwarshesh@gmail.com>
2024-09-22 17:12:55 +0530

    Matrix Transpose

---
+++ Java/Array/Array-medium/MatrixTranspose.java
@@ -0,0 +1,36 @@
+/**
+ * MatrixTranspose
+ */
+public class MatrixTranspose {
+    public static void main(String[] args) {
+        int matrix[][] = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        System.out.println("Original Matrix");
+        display(matrix);
+        System.out.println();
+
+        System.out.println("Transposed Matrix");
+        display(transpose(matrix));
+
+    }
+
+    public static int[][] transpose(int matrix[][]) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] transposed = new int[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+
+    public static void display(int transposed[][]) {
+        for (int i = 0; i < transposed.length; i++) {
+            for (int j = 0; j < transposed[0].length; j++) {
+                System.out.print(transposed[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
