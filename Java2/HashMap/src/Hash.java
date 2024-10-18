b9b0068abdde196729c74f6ad786286bd842b718
Umesh Nagare <umeshnagare27@gmail.com>
2024-10-18 23:58:30 +0530

    Hash Maps

---
+++ Java2/HashMap/src/Hash.java
@@ -0,0 +1,11 @@
+public class Hash {
+    public static void main(String[] args) {
+        String name = "Umesh";
+        String name2 = "Yogesh";
+        int hashcode = name.hashCode();
+        int hashcode2 = name2.hashCode();
+        System.out.println(hashcode);
+        System.out.println(hashcode2);
+
+    }
+}
