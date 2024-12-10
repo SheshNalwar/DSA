9a31f11617e588354940fc2ea8ae9786cc3cc7d7
SheshNalwar <nalwarshesh@gmail.com>
2024-12-11 00:57:59 +0530

    Isomorphic Strings

---
+++ Top150_DSA/Arrays&Strings/IsomorphicString.java
@@ -0,0 +1,51 @@
+/*
+ * 205. Isomorphic Strings
+
+Given two strings s and t, determine if they are isomorphic.
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+Example 1:
+Input: s = "egg", t = "add"
+Output: true
+Explanation:
+The strings s and t can be made identical by:
+Mapping 'e' to 'a'.
+Mapping 'g' to 'd'.
+
+Example 2:
+Input: s = "foo", t = "bar"
+Output: false
+Explanation:
+The strings s and t can not be made identical as 'o' needs to be mapped to both 'a' and 'r'.
+ */
+
+public class IsomorphicString {
+    public static void main(String[] args) {
+        String s = "egg";
+        String t = "add";
+        System.out.println(isIsomorphic(s, t));
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        boolean isomorphic = false;
+
+        int map1[] = new int[200];
+        int map2[] = new int[200];
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            if (map1[s.charAt(i)] != map2[t.charAt(i)]) {
+                return false;
+            }
+            map1[s.charAt(i)] = i + 1;
+            map2[t.charAt(i)] = i + 1;
+
+        }
+        return true;
+
+    }
+
+}
