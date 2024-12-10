f20c720398013b69cfe8a7f923e0afbba7f58b2b
SheshNalwar <nalwarshesh@gmail.com>
2024-12-10 13:18:51 +0530

    Longest Common prefix

---
+++ Top150_DSA/Arrays&Strings/LongestCommonPrefix.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+public class LongestCommonPrefix {
+
+    public static void main(String[] args) {
+        String[] strs = { "flower", "flow", "flight" };
+        System.out.println(longestCommonPrefix(strs));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        StringBuilder str = new StringBuilder();
+
+        Arrays.sort(strs);
+        char[] first = strs[0].toCharArray();
+        char[] last = strs[strs.length - 1].toCharArray();
+
+        for (int i = 0; i < first.length; i++) {
+            if (first[i] != last[i]) {
+                break;
+            }
+            str.append(first[i]);
+        }
+
+        return str.toString();
+    }
+}
