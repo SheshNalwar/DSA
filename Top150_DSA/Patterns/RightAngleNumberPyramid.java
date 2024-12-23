26c8c5ea9260aaa1e37e162858cccc5ae1beee28
SheshNalwar <nalwarshesh@gmail.com>
2024-12-23 12:34:28 +0530

    Right angle number pyramid

---
+++ Top150_DSA/Patterns/RightAngleNumberPyramid.java
@@ -0,0 +1,17 @@
+
+public class RightAngleNumberPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+
+        }
+    }
+}
