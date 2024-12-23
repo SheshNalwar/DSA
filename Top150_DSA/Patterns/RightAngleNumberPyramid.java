8b50349051718e1d774616ab89edad1f6331869a
SheshNalwar <nalwarshesh@gmail.com>
2024-12-23 12:43:28 +0530

    Inverted Right star pyramid

--- Top150_DSA/Patterns/RightAngleNumberPyramid.java
+++ Top150_DSA/Patterns/RightAngleNumberPyramid.java
@@ -1,4 +1,20 @@
+/*
+ * 
+ *Input Format: N = 3
+Result: 
+1
+1 2 
+1 2 3
 
+Input Format: N = 6
+Result:
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+ */
 public class RightAngleNumberPyramid {
     public static void main(String[] args) {
         int n = 5;
