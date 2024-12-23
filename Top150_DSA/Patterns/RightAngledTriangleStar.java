9cdf4505b7f04b72bf9e07090bec41ed7cb14eb7
SheshNalwar <nalwarshesh@gmail.com>
2024-12-23 12:31:33 +0530

    Right angled triangle star pattern

---
+++ Top150_DSA/Patterns/RightAngledTriangleStar.java
@@ -0,0 +1,32 @@
+/*
+ * 
+Input Format: N = 3
+Result: 
+* 
+* * 
+* * *
+
+Input Format: N = 6
+Result:
+* 
+* * 
+* * *
+* * * *
+* * * * *
+* * * * * *
+ */
+public class RightAngledTriangleStar {
+    public static void main(String[] args) {
+        int n = 5;
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
