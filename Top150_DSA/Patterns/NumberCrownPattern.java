9523b8f9362bcf22362481a4fb45e20e5a00ac55
SheshNalwar <nalwarshesh@gmail.com>
2024-12-27 13:37:23 +0530

    Number crown pattern

---
+++ Top150_DSA/Patterns/NumberCrownPattern.java
@@ -0,0 +1,39 @@
+/*
+ * 
+ Input Format: N = 3
+Result: 
+1    1
+12  21
+123321
+
+Input Format: N = 6
+Result:   
+1          1
+12        21
+123      321
+1234    4321
+12345  54321
+123456654321
+ */
+
+public class NumberCrownPattern {
+    public static void main(String[] args) {
+        int n = 6;
+        int spaces = 2 * (n - 1);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            for (int j = 1; j <= spaces; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            System.out.println();
+            spaces -= 2;
+        }
+    }
+}
