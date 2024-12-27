da60b4f6a06a66ed76530399b45d7159b89a8f30
SheshNalwar <nalwarshesh@gmail.com>
2024-12-27 15:24:21 +0530

    Increasing number triangle

---
+++ Top150_DSA/Patterns/IncreasingNumberTrianglePattern.java
@@ -0,0 +1,31 @@
+/*
+ * 
+ Input Format: N = 3
+Result: 
+1
+2 3
+4 5 6
+
+Input Format: N = 6
+Result:   
+1
+2  3
+4  5  6
+7  8  9  10
+11  12  13  14  15
+16  17  18  19  20  21
+ */
+
+public class IncreasingNumberTrianglePattern {
+    public static void main(String[] args) {
+        int n = 3;
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num + " ");
+                num += 1;
+            }
+            System.out.println();
+        }
+    }
+}
