2dea0563d8a2ffef5151a83aa180ce80a064b29a
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-03 12:44:54 +0530

    10 patterns

--- Java2/src/Patterns.java
+++ Java2/src/Patterns.java
@@ -1,6 +1,6 @@
 public class Patterns {
     public static void main(String[] args) {
-        pattern7(5);
+        pattern10(5);
     }
     static void pattern1(int n){
         for (int i = 0; i < n; i++) {
@@ -51,9 +51,40 @@ public class Patterns {
         }
     }
     static void pattern7(int n){
-        for (int i = n; i >=1; i--) {
+        for (int i = 0; i < 2*n; i++) {
+            int col=i>n?2*n-i-1:i;
+            for (int j = 0; j < col; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern8(int n){
+        int start =1;
+        for (int i = 0; i < n; i++) {
+            if (i%2==0) start=0;
+            else start=1;
+            for (int j = 0; j <= i; j++) {
+                System.out.print(start);
+                start=1-start;
+            }
+            System.out.println();
+        }
+    }
+    static void pattern9(int n){
+        int num = 1;
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(num + " ");
+                num=num+1;
+            }
+            System.out.println();
+        }
+    }
+    static void pattern10(int n){
+        for (int i = 0; i <= n; i++) {
+            for (char ch='A';ch<='A'+i ;ch++){
+                System.out.print(ch + " ");
             }
             System.out.println();
         }
