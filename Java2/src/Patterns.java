f650a64fa96ec910be4d89981212a63efbb7b9ef
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-07 23:57:56 +0530

    pattern

--- Java2/src/Patterns.java
+++ Java2/src/Patterns.java
@@ -10,6 +10,7 @@ public class Patterns {
             System.out.println();
         }
     }
+    
     static void pattern2(int n){
         for (int i = 1; i <= n; i++) {
             for (int j = 0; j < i; j++) {
