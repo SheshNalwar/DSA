9086651ff7ddf531279f954142369ec8561c670a
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-11 14:37:02 +0530

    reverse an array using recursion

--- Java2/src/Recursion.java
+++ Java2/src/Recursion.java
@@ -1,7 +1,9 @@
+import java.util.Arrays;
+
 public class Recursion {
     public static void main(String[] args) {
-        int sum = sum(5);
-        System.out.println(sum);
+        int ar[]={1,2,3,4,5};
+        System.out.println(Arrays.toString(reverseArray(ar,0, ar.length-1)));
     }
     static void oneton(int n){
         if (n == 0) {
@@ -22,6 +24,20 @@ public class Recursion {
             return 0;
         }
         return n + sum(n-1);
+    }
+    static int[] reverseArray(int ar[],int start,int end){
+        if (start>end){
+            return ar;
+        }
+        swap(ar, start, end);
+        reverseArray(ar, start+1, end-1);
+        return ar;
+    }
+    static int[] swap(int ar[],int i,int j){
+        int temp = ar[i];
+        ar[i]=ar[j];
+        ar[j]=temp;
+        return ar;
     }
 
 }
