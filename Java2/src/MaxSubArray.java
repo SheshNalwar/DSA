489ddfa2fd9b1d83932258f82a44ce0c3dcaf9ed
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-11 17:23:58 +0530

    sub array and sub string corrected one

---
+++ Java2/src/MaxSubArray.java
@@ -0,0 +1,13 @@
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int ar[]={-2,1,-3,4,-1,2,1,-5,4};
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = i; j < ar.length; j++) {
+
+            }
+        }
+    }
+//    static int maxSubArray(int ar[]){
+//
+//    }
+}
--- Java2/src/Sub.java
+++ Java2/src/Sub.java
@@ -1,24 +1,29 @@
 public class Sub {
     public static void main(String[] args) {
         int arr[]={1,2,3,4,5};
+//        subArrays(arr);
         String name = "Umesh";
-        subArrays(arr);
         subStrings(name);
     }
     static void subStrings(String s){
         for (int i = 0; i < s.length(); i++) {
-            for (int j = i; j <s.length() ; j++) {
-                System.out.print(s.charAt(j));
+            for (int j = i; j < s.length() ; j++) {
+                for (int k = i; k <=j ; k++) {
+                    System.out.print(s.charAt(k));
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
-    static void subArrays(int arr[]){
+    static void subArrays(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
-            for (int j = i; j < arr.length ; j++) {
-                System.out.print(arr[j]);
+            for (int j = i; j < arr.length; j++) {
+                for (int k = i; k <= j; k++) {
+                    System.out.print(arr[k] + " ");
+                }
+                System.out.println();
             }
-            System.out.println();
         }
     }
+
 }
