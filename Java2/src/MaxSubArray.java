c46de9171bac575eed8de101d8ec2793636e6165
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-12 12:49:10 +0530

    Max Sub Array Sum

--- Java2/src/MaxSubArray.java
+++ Java2/src/MaxSubArray.java
@@ -1,13 +1,18 @@
 public class MaxSubArray {
     public static void main(String[] args) {
         int ar[]={-2,1,-3,4,-1,2,1,-5,4};
-        for (int i = 0; i < ar.length; i++) {
-            for (int j = i; j < ar.length; j++) {
-
-            }
-        }
+        System.out.println(maxSubArray(ar));
     }
-//    static int maxSubArray(int ar[]){
-//
-//    }
+    static int maxSubArray(int ar[]){
+        int sum=ar[0];
+        int max=ar[0];
+        for (int i = 0; i < ar.length; i++) {
+            if (sum<0){
+                sum=0;
+            }
+            sum+=ar[i];
+            max=Math.max(max,sum);
+        }
+        return max;
+    }
 }
