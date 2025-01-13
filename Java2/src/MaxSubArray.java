0d87f9201c7307e04292fa2a66a1a47a20eec579
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-13 20:11:09 +0530

    remove duplicates from an array

--- Java2/src/MaxSubArray.java
+++ Java2/src/MaxSubArray.java
@@ -1,10 +1,7 @@
 public class MaxSubArray {
     public static void main(String[] args) {
         int ar[]={-2,1,-3,4,-1,2,1,-5,4};
-        int array[]={1,2,3,4,5};
-//        System.out.println(maxSubArray(ar));
-        int max = bruteForcemaxSubArray(ar);
-        System.out.println(max);
+        System.out.println(bruteForcemaxSubArray(ar));
     }
     static int maxSubArray(int ar[]){
         int sum=ar[0];
@@ -19,7 +16,7 @@ public class MaxSubArray {
         return max;
     }
     static int bruteForcemaxSubArray(int ar[]){
-        int max=0;
+        int max=Integer.MIN_VALUE;
         for (int i = 0; i < ar.length; i++) {
             for (int j = 0; j < ar.length; j++) {
                 int sum=0;
