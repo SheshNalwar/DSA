8d4bae165dd1d53e66ba7398bbef022285e2868d
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-13 19:19:17 +0530

    Max Sub Array Brute Force

--- Java2/src/MaxSubArray.java
+++ Java2/src/MaxSubArray.java
@@ -1,7 +1,10 @@
 public class MaxSubArray {
     public static void main(String[] args) {
         int ar[]={-2,1,-3,4,-1,2,1,-5,4};
-        System.out.println(maxSubArray(ar));
+        int array[]={1,2,3,4,5};
+//        System.out.println(maxSubArray(ar));
+        int max = bruteForcemaxSubArray(ar);
+        System.out.println(max);
     }
     static int maxSubArray(int ar[]){
         int sum=ar[0];
@@ -12,6 +15,19 @@ public class MaxSubArray {
             }
             sum+=ar[i];
             max=Math.max(max,sum);
+        }
+        return max;
+    }
+    static int bruteForcemaxSubArray(int ar[]){
+        int max=0;
+        for (int i = 0; i < ar.length; i++) {
+            for (int j = 0; j < ar.length; j++) {
+                int sum=0;
+                for (int k = i; k <=j ; k++) {
+                    sum+=ar[k];
+                }
+                max=Math.max(sum,max);
+            }
         }
         return max;
     }
