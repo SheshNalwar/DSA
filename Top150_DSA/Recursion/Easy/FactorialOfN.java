0c1613d2030579616858dcd06ece21197b8b59e0
SheshNalwar <nalwarshesh@gmail.com>
2025-01-16 22:54:27 +0530

    Factorial of a number using recursion

---
+++ Top150_DSA/Recursion/Easy/FactorialOfN.java
@@ -0,0 +1,15 @@
+public class FactorialOfN {
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.println(factorial(n));
+    }
+
+    public static int factorial(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+}
