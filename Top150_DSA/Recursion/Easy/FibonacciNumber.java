7958f3861ac76e7446bc0606191c7552eac97f7b
SheshNalwar <nalwarshesh@gmail.com>
2025-01-16 23:21:10 +0530

    Fibonacci series

---
+++ Top150_DSA/Recursion/Easy/FibonacciNumber.java
@@ -0,0 +1,16 @@
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i <= n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+
+    public static int fibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
