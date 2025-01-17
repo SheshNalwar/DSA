5bdcfd280b44b53af14bfb4aac6d4824c264d1b8
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-17 15:41:37 +0530

    Longest Common Prefix

---
+++ Java2/src/LongestCommonPrefix.java
@@ -0,0 +1,23 @@
+public class LongestCommonPrefix {
+    public static void main(String[] args) {
+        String[] strs = {"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+    static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0) return "";
+        String pref = strs[0];
+        int prefLen = pref.length();
+        for (int i = 1; i < strs.length; i++) {
+            String s = strs[i];
+            while (prefLen > s.length() || !pref.equals(s.substring(0, prefLen))) {
+                prefLen--;
+                if (prefLen == 0) {
+                    return "";
+                }
+                pref = pref.substring(0, prefLen);
+            }
+        }
+
+        return pref;
+    }
+}
