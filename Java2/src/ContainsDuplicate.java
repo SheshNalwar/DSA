07e98ee106be02f17c570f278072e233733e1758
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-19 15:41:30 +0530

    Contains Duplicate using Set

--- Java2/src/ContainsDuplicate.java
+++ Java2/src/ContainsDuplicate.java
@@ -1,9 +1,12 @@
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class ContainsDuplicate {
     public static void main(String[] args) {
-        int arr[]={1,2,3,1};
-        System.out.println(isDuplicate(arr));
+        int arr[]={1,2,3,4};
+        int arr2[]={1,2,3,2};
+        System.out.println(isDuplicate2(arr));
+        System.out.println(isDuplicate2(arr2));
     }
     static boolean isDuplicate(int []arr){
         Arrays.sort(arr);
@@ -11,6 +14,16 @@ public class ContainsDuplicate {
             if (arr[i] == arr[i - 1]) {
                 return true;
             }
+        }
+        return false;
+    }
+    static boolean isDuplicate2(int []arr){
+        HashSet<Integer> hashSet = new HashSet<>();
+        for (int i :arr) {
+            if (hashSet.contains(i)){
+                return true;
+            }
+            hashSet.add(i);
         }
         return false;
     }
---
+++ Java2/src/Hashing.java
@@ -0,0 +1,6 @@
+public class Hashing {
+    public static void main(String[] args) {
+        int array[]={10,5,10,15,10,5};
+    }
+
+}
