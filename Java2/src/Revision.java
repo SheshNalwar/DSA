be4ceb7243b6f768b50513ecac80d49cfe9a1c15
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-23 10:14:43 +0530

    Revision 4 questions

---
+++ Java2/src/Revision.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+
+public class Revision {
+    public static void main(String[] args) {
+        int array[]={1,2,3,4,5};
+        int array2[]={1,2,3,5,4};
+        String s = "fried";
+        String t = "fired";
+//        System.out.println(linearSearch(array,4));
+//        System.out.println(sortedorNotArray(array));
+//        System.out.println(Arrays.toString(reverseArray(array)));
+//        System.out.println(isAnagram(s,t));
+
+    }
+    static int linearSearch(int ar[],int target){
+        for (int i = 0; i < ar.length; i++) {
+            if (ar[i]==target){
+                System.out.print("Found at index ");
+                return i;
+            }
+        }
+        return -1;
+    }
+    static boolean sortedorNotArray(int ar[]){
+        for (int i = 0; i < ar.length-1; i++) {
+            if (ar[i]>ar[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    static int[] reverseArray(int ar[]){
+        int start = 0;
+        int end = ar.length-1;
+        while (start<end){
+            int temp = ar[start];
+            ar[start]=ar[end];
+            ar[end]=temp;
+            start++;
+            end--;
+        }
+        return ar;
+    }
+    static  boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+        return Arrays.equals(sChars, tChars);
+    }
+}
