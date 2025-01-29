90d7e347c0230dc5bf70369bcc8a767bb7b98cdf
SheshNalwar <nalwarshesh@gmail.com>
2025-01-30 00:07:56 +0530

    next permutation optimized

--- Top150_DSA/Revision/NextPermutation.java
+++ Top150_DSA/Revision/NextPermutation.java
@@ -1,12 +1,15 @@
+import java.util.Arrays;
+
 public class NextPermutation {
 
     public static void main(String[] args) {
-        int arr[] = { 3, 1, 2 };
+        int arr[] = { 1, 2, 3 };
 
         nextPermutation(arr);
+        System.out.println(Arrays.toString(arr));
     }
 
-    public static void nextPermutation(int[] nums) {
+    public static int[] nextPermutation(int[] nums) {
         // find the breakpoint where a[i] < a[i+1]
         // find the index where the element is just greater than a[i] and is smallest in
         // the remaining array
@@ -14,19 +17,30 @@ public class NextPermutation {
         // reverse the remaining array
 
         if (nums == null || nums.length <= 1)
-            return;
+            return nums;
 
-        int i = nums.length - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1])
-            i--;
-        if (i >= 0) {
-            int j = nums.length - 1;
-            while (nums[j] <= nums[i])
-                j--;
-            swap(nums, i, j);
+        int idx = -1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                idx = i;
+                break;
+            }
         }
-        reverse(nums, i + 1, nums.length - 1);
 
+        if (idx == -1) {
+            reverse(nums, 0, nums.length - 1);
+            return nums;
+        }
+
+        for (int j = nums.length - 1; j >= idx; j--) {
+            if (nums[j] > nums[idx]) {
+                swap(nums, idx, j);
+            }
+        }
+
+        reverse(nums, idx + 1, nums.length - 1);
+
+        return nums;
     }
 
     public static void swap(int nums[], int i, int j) {
