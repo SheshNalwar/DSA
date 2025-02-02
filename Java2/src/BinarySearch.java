d41435f618a243a984eed341cdd3c7613be0450a
Umesh Nagare <umeshnagare27@gmail.com>
2025-02-02 13:02:42 +0530

    Binary Search Revision

--- Java2/src/BinarySearch.java
+++ Java2/src/BinarySearch.java
@@ -2,7 +2,7 @@
 public class BinarySearch {
     public static void main(String[] args) {
         int ar[]= {1,2,3,4,5};
-        System.out.println(binarySearch(ar,4));
+        System.out.println(binarySearchRevision(ar,4));
     }
     static int binarySearch(int[] ar,int target){
         int start = 0;
@@ -18,5 +18,17 @@ public class BinarySearch {
             }
         }
         return -1;
+    }
+    static int binarySearchRevision(int[] ar,int target){
+        int start=0;
+        int end = ar.length-1;
+        while (start<=end){
+            int mid = (start+end)/2;
+            if (ar[mid]==target)return mid;
+            else if (ar[mid]<target) {
+                start=mid+1;
+            }else end=mid-1;
+        }
+        return -2;
     }
 }
--- Java2/src/CountFrequency.java
+++ Java2/src/CountFrequency.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.util.HashMap;
 
 public class CountFrequency {
