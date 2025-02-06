0f310916eac4f30c9e5efd949f1c44c8436afa42
Umesh Nagare <umeshnagare27@gmail.com>
2025-02-06 12:59:57 +0530

    count digits revision

--- Java2/src/CountDigits.java
+++ Java2/src/CountDigits.java
@@ -1,7 +1,7 @@
 public class CountDigits {
     public static void main(String[] args) {
         int n = 1234521825;
-        System.out.println(count(n));
+        System.out.println(countRevision(n));
     }
     static int count(int n){
         int totalCount=0;
@@ -10,5 +10,13 @@ public class CountDigits {
             totalCount++;
         }
         return totalCount;
+    }
+    static int countRevision(int n){
+        int count = 0;
+        while (n!=0){
+            n=n/10;
+            count++;
+        }
+        return count;
     }
 }
