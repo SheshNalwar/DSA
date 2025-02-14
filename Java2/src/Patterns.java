9d457b45a034109683fe19ba9dac9c44c7a72608
Umesh Nagare <umeshnagare27@gmail.com>
2025-02-14 21:44:25 +0530

    7 patterns

--- Java2/src/Patterns.java
+++ Java2/src/Patterns.java
@@ -1,3 +1,63 @@
 public class Patterns {
-
+    public static void main(String[] args) {
+        pattern7(5);
+    }
+    static void pattern1(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern2(int n){
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void pattern3(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    static void pattern4(int n){
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+    static void pattern5(int n){
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+    static void pattern6(int n){
+        int num=1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+    static void pattern7(int n){
+        for (int i = 0; i < n; i++) {
+            for (char c='A';c<='A'+i;c++ ) {
+                System.out.print(c+" ");
+            }
+            System.out.println();
+        }
+    }
 }
