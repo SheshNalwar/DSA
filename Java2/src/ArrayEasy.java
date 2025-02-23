e74ce2a96338ace71fbb03b6574c61e1e74e1847
Umesh Nagare <umeshnagare27@gmail.com>
2025-02-23 16:13:30 +0530

    2 Array que

--- Java2/src/ArrayEasy.java
+++ Java2/src/ArrayEasy.java
@@ -1,14 +1,23 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class ArrayEasy {
+//    Declarations
+    static ArrayList<Integer> arrayList = new ArrayList<>();
+    static HashMap<Integer,Integer> hashMap = new HashMap<>();
+    static int array[]={1,2,3,4,5};
+    static int array2[]={2,3,4,4,5};
+    static int appears[]={2,2,1};
+    static int zeroes[]={1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
+    static int duplicates[]={1,1,1,2,2,3,3,3,3,4,4};
+
+//    Main
     public static void main(String[] args) {
-        int array[]={1,2,3,4,5,6,7};
-//        int duplicates[]={1,1,1,2,2,3,3,3,3,4,4};
-//        System.out.println(removeDuplicates(duplicates));
-//        System.out.println(Arrays.toString(leftRotateArray(array)));
-        System.out.println(rotateByK(array,2));
+
     }
+
+//    Methods
     static int largest(int ar[]){
         int largest = Integer.MIN_VALUE;
         for (int i = 0; i < ar.length; i++) {
@@ -45,7 +54,6 @@ public class ArrayEasy {
         System.out.println("Second Smallest "+secondSmallest);
     }
     static ArrayList<Integer> removeDuplicates(int ar[]){
-        ArrayList<Integer> arrayList = new ArrayList<>();
         for (int i = 0; i < ar.length; i++) {
             if (!arrayList.contains(ar[i]))arrayList.add(ar[i]);
         }
@@ -60,7 +68,6 @@ public class ArrayEasy {
         return ar;
     }
     static ArrayList<Integer> rotateByK(int ar[],int k){
-        ArrayList<Integer> arrayList = new ArrayList<>();
         for (int i = ar.length-k; i < ar.length ; i++) {
             arrayList.add(ar[i]);
         }
@@ -69,4 +76,31 @@ public class ArrayEasy {
         }
         return arrayList;
     }
+    static int[] rotateByK2(int ar[],int k){
+        //TODO : incomplete
+        int temp[]=new int[k];
+        for (int i = ar.length-k; i < ar.length ; i++) {
+            temp[i]=ar[i];
+        }
+        for (int i = k; i < ar.length-k ; i++) {
+            
+        }
+        return ar;
+    }
+    static int[] moveAllZeros(int ar[]){
+        for (int i = 0; i < ar.length; i++) {
+            if (ar[i]!=0)arrayList.add(ar[i]);
+        }
+        for (int i = arrayList.size(); i <ar.length ; i++) {
+            arrayList.add(0);
+        }
+        return ar;
+    }
+    static int missingNumber(int ar[]){
+        for (int i = 1; i <= ar.length; i++) {
+            if (ar[i]!=ar[i-1]+1)return ar[i]-1;
+        }
+        return -1;
+    }
+
 }
